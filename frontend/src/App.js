e46b984500fdf709daec144859a92cf4ef7225b6
echonorthernlights <elhassanianas@gmail.com>
2023-07-06 21:17:05 +0100

    router setup + HomeScreen design + components(Product, Header, Footer) + dummy data for products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,19 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+import { Outlet } from "react-router-dom";
+
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
