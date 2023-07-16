be69c222551022ba142d2de1a06c2ce895511b76
echonorthernlights <elhassanianas@gmail.com>
2023-07-16 18:12:08 +0100

    products slice + webpack fix

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -2,26 +2,37 @@ import React, { useEffect, useState } from "react";
 import { Row, Col } from "react-bootstrap";
 import axios from "axios";
 import Product from "../components/Product";
-
+import { useGetProductsQuery } from "../slices/productsApiSlice.js";
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  /*const [products, setProducts] = useState([]);
   useEffect(() => {
     const fetchProducts = async () => {
       const { data } = await axios.get("/api/products");
       setProducts(data);
     };
     fetchProducts();
-  }, []);
+  }, []);*/
+  const { data: products, isLoading, error } = useGetProductsQuery();
+
   return (
     <>
-      <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {isLoading ? (
+        <h1>Loading ...</h1>
+      ) : error ? (
+        <h1>{error.error}</h1>
+      ) : (
+        <>
+          {" "}
+          <h1>Latest Products</h1>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
   );
 };
