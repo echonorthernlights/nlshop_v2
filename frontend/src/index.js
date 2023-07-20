5b92ed50897661aa1a6470a7bb1f8da30ead20cd
echonorthernlights <elhassanianas@gmail.com>
2023-07-20 21:15:26 +0100

    components (LoginScreen + ForContainer) + authSlice + usersApiSlice + added login screen route + store (added authSlice)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -18,11 +18,13 @@ import App from "./App";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
       <Route path="/cart" element={<CartScreen />} />
     </Route>
---
+++ frontend/src/slices/authSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  userInfo: localStorage.getItem("userInfo")
+    ? JSON.parse(localStorage.getItem("userInfo"))
+    : null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setCredentials: (state, action) => {
+      state.userInfo = action.payload;
+      localStorage.setItem("userInfo", JSON.stringify(action.payload));
+    },
+  },
+});
+
+// setCredentials exported as an action so we can call and use it
+export const { setCredentials } = authSlice.actions;
+
+// authSlice reducer
+export default authSlice.reducer;
