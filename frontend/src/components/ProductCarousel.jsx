3f9867ebb5da8855baebc8ca266508e400f6e930
echonorthernlights <elhassanianas@gmail.com>
2023-08-03 21:47:48 +0100

    Minor fix 3

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -5,8 +5,10 @@ import Message from "./Message";
 import { useGetTopProductsQuery } from "../slices/productsApiSlice";
 
 const ProductCarousel = () => {
-  const { data: products, error } = useGetTopProductsQuery();
-  return error ? (
+  const { data: products, isLoading, error } = useGetTopProductsQuery();
+  return isLoading ? (
+    <Loader />
+  ) : error ? (
     <Message variant="danger">{error}</Message>
   ) : (
     <Carousel pause="hover" className="bg-primary mb-4">
